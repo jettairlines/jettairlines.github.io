86d9a4b2e482ff5043e9cd371ff5dee9d65db0da
jettairlines <lwanghe2@gmail.com>
2018-05-30 17:15:10 -0700

    tryna fix build

--- src/Noun.js
+++ src/Noun.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 const nounImg = {
-          "jett": ["Jett Wang", "jett.png"],
-          "htmlcss": ["HTML CSS", "htmlcss.svg"],
-          "boostrap": ["BootstrapCSS", "boostrap.png"]
-      }
+    "jett": ["Jett Wang", "jett.png"],
+    "htmlcss": ["HTML CSS", "htmlcss.svg"],
+    "boostrap": ["BootstrapCSS", "boostrap.png"],
+    "sass" : ["SaSS","sass.png"],
+    "jquery" : ["jQuery","jquery.png"],
+    "react" : ["ReactJS","react.png"],
+    "sketch" : ["Sketch","sketch.png"],
+    "photoshop" : ["Photoshop","photoshop.png"],
+    "illustrator" : ["Illustrator","illustrator.png"],
+    "indesign" : ["Indesign","indesign.png"],
+    "origami" : ["Origami Studio","origami.png"],
+    "flinto" : ["Flinto","flinto.jpg"],
+    "invision" : ["Invision","invision.jpg"],
+    "marvel" : ["Marvel","marvelapp.png"],
+    "pop" : ["POP","pop.png"],
+    "omni" : ["Omnigraffle","omni.png"],
+    "wix" : ["Wix","wix.png"],
+    "weebly" : ["Weebly","weebly.png"],
+    "webflow" : ["Webflow","webflow.jpg"],
+    "squarespace" : ["Squarespace","squarespace.svg"],
+    "firebase" : ["FirebaseDB","firebase."]
+    
+}
 
 export class Noun extends Component {
     render(){
